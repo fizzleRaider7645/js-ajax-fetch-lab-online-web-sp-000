1759c557fe1f5ed040a69e0acb80e0e07f9b09a8
Douglas Uretsky <douglasuretsky@gmail.com>
2019-03-10 17:33:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,5 +21,20 @@ function showResults(json) {
 }
 
 function createIssue() {
-  
+  const repo = `${user}/js-ajax-fetch-lab`;
+  const url = `${baseURL}/repos/${repo}/issues`;
+  const postData = {
+    title: document.getElementById('title').value,
+    body: document.getElementById('body').value
+  };
+
+  fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(postData),
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
+    .then(res => res.json())
+    .then(json => getIssues());
 }
